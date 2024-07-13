f611487be292628da3e16ae416614be1948b5ea2
hieuhuynh <hieuhuynhif@gmail.com>
2024-07-13 09:56:26 +0700

    setup

--- src/problem2/faccy-form/src/components/FancyForm/index.tsx
+++ src/problem2/faccy-form/src/components/FancyForm/index.tsx
@@ -1,7 +1,13 @@
+import { currency } from "../../seed";
+
 type Props = {
   title: string;
 };
 
 export default function Fancyform({ title }: Props) {
+
+  const currencies = currency
+  console.log(currencies)
+
   return <div>Fancyform:{title}</div>;
 }
